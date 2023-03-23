6dda69ca8fae465290895b6801acd8e1b501d5f0
Alex Matsui <oleksii.matsui@gmail.com>
2023-03-23 12:35:25 +0200

    Add project files.

---
+++ PWATest/wwwroot/js/site.js
@@ -0,0 +1,30 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+
+
+let deferredPrompt;
+const addBtn = document.querySelector(".add-button");
+addBtn.style.display = "none";
+
+window.addEventListener("beforeinstallprompt", (e) => {
+    e.preventDefault();
+    deferredPrompt = e;
+    addBtn.style.display = "block";
+
+    addBtn.addEventListener("click", (e) => {
+        addBtn.style.display = "none";
+        deferredPrompt.prompt();
+
+        deferredPrompt.userChoice.then((choiceResult) => {
+            if (choiceResult.outcome === "accepted") {
+                console.log("User accepted the A2HS prompt");
+            } else {
+                console.log("User dismissed the A2HS prompt");
+            }
+            deferredPrompt = null;
+        });
+    });
+});
